d80f7815fc2759e9ce08c90c3eecd513b0e0952a
Pamperin, Clint A <c.pamperin21@gmail.com>
2017-03-01 18:04:49 -0600

    add: overwrite and file checks

--- src/index.ts
+++ src/index.ts
@@ -14,37 +14,52 @@ argManager.on(new Argument('pretty', 'p', 2), (value, defaultValue) => {
     prettySpace = parsed !== NaN ? parsed : defaultValue;
 });
 
+var overwrite: boolean = false;
+argManager.on(new Argument('overwrite', 'o'), () => {
+    overwrite = true;
+});
+
 var outFile: string = '',
     inFile: string = '';
 argManager.onDefault((defaults) => {
     if (defaults.length < 2) { 
         console.log('Error: InFile and OutFile required.');
-        console.log('Ex:' + '\x1b[41m\x1b[33mlljson path/to/in/file.txt outFileName [-p, --pretty=2]\x1b[0m');
+        console.log('Ex:' + '\x1b[41m\x1b[33mlljson path/to/in/file.txt outFileName [-p, --pretty=2] [-o, --overwrite]\x1b[0m');
         process.exit();
     }
 
     inFile = defaults[0];
     outFile = defaults[1];
+    if (outFile.indexOf('.json') === -1) {
+        outFile = outFile + '.json';
+    }
+});
 
-    var lineReader = createInterface({
-        input: fs.createReadStream(inFile)
-    });
+if (!fs.existsSync(inFile)) {
+    console.log('InFile does not exist: ' + inFile);
+    process.exit();
+}
 
-    var newArray: string[] = [];
-    lineReader.on('line', (line: string) => {
-        newArray.push(line);
-    });
+if (!overwrite && fs.existsSync(outFile)) {
+    console.log('OutFile already exists: ' + outFile);
+    console.log('Use [-o, --overwrite] to overwrite the file.');
+    process.exit();
+}
 
-    lineReader.on('close', () => {
-        if (outFile.indexOf('.json') === -1) {
-            outFile = outFile + '.json';
-        }
+var lineReader = createInterface({
+    input: fs.createReadStream(inFile)
+});
 
-        var json: string = pretty ? JSON.stringify(newArray, null, prettySpace) : JSON.stringify(newArray);
+var newArray: string[] = [];
+lineReader.on('line', (line: string) => {
+    newArray.push(line);
+});
 
-        fs.writeFile(outFile, json, (err) => {
-            if      (err) console.log(err);
-            else    console.log('JSON outputed to ./' + outFile);
-        });
+lineReader.on('close', () => {
+    var json: string = pretty ? JSON.stringify(newArray, null, prettySpace) : JSON.stringify(newArray);
+
+    fs.writeFile(outFile, json, (err) => {
+        if      (err) console.log(err);
+        else    console.log('JSON outputed to ./' + outFile);
     });
 });
